a40be8368f2416732ecff4c1698e30d7b2544559
akshay-puranik <akshay.social@gmail.com>
2022-07-23 16:30:51 +0530

    updates

--- akshay/donation_page.js
+++ akshay/donation_page.js
@@ -5,7 +5,8 @@ let obj = {
   image1: "https://cimages.milaap.org/milaap/image/upload/c_fill,g_faces,h_452,w_603/v1656590899/production/images/campaign/523508/WhatsApp_Image_2022-06-30_at_5.05.30_PM_seyrm4_1656590902.jpg",
   image2: "https://cimages.milaap.org/milaap/image/upload/c_fill,g_faces,h_452,w_603/v1656787035/production/images/campaign/523508/WhatsApp_Image_2022-07-01_at_12.12.06_AM_gyu2j8_1656787038.jpg",
   name: "Assam Needs Your Help To Set Up Medical Camps",
-  price: "3,00,000",
+  price: "300000",
+  donated: "300000"
 }
 
 let title = document.querySelector("#fr_title");
@@ -24,3 +25,30 @@ setInterval(() => {
 
 slide.innerHTML = null;
 slide.append(img);
+
+
+let price = obj.price
+
+main();
+
+function main() {
+
+  console.log("clicked")
+  let donated = +obj.donated;
+
+
+  let percentage = Math.floor((donated / price) * 100)
+  console.log(percentage)
+  let x = 156 - (156 * (percentage / 100))
+  var style = document.createElement('style');
+  // style.type = 'text/css';
+  var keyFrames = `\
+    @keyframes anim {\
+        100% {\
+            stroke-dashoffset: ${x};\
+        }\
+    }\
+    `;
+  style.innerHTML = keyFrames.replace(/x/, "0");
+  document.getElementsByTagName('head')[0].appendChild(style);
+}

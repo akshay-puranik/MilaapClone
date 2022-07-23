a761eb8586c78c9b65bd64e6c33ae809ee2832fd
akshay-puranik <akshay.social@gmail.com>
2022-07-23 20:18:51 +0530

    more updates

--- components/login_stript.js
+++
@@ -1,6 +0,0 @@
-  var click_count = 0;
-  document.querySelector("#user_logo_sin").addEventListener("click", () => {
-    if (click_count % 2 == 0) document.querySelector("#login_signup").style.display = "block";
-    else document.querySelector("#login_signup").style.display = "none";
-    click_count++;
-  })
